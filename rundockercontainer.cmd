beb942bc8134847074b46dc5e0dd95204305b969
BR39LH <knalawad@tibco.com>
2018-03-01 21:48:27 +0100

    asdad

--- rundockercontainer.cmd
+++ rundockercontainer.cmd
@@ -3,4 +3,4 @@ SET DOCKER_HOST=tcp://192.168.99.100:2376
 SET DOCKER_CERT_PATH=C:/Users/BR39LH.AD/.docker/machine/machines/default
 SET DOCKER_MACHINE_NAME=default
 SET COMPOSE_CONVERT_WINDOWS_PATHS=true
-docker run -p 8080:8080 knalawad/spring-petclinic
+docker run -p 8080:8080 knalawad/springboot-petclinic
